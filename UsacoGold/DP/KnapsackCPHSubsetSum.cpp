3c3e621eb1ffd6d20ac375e57eaa278414579216
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-06 17:12:33 +0530

    Create KnapsackCPHSubsetSum.cpp

---
+++ UsacoGold/DP/KnapsackCPHSubsetSum.cpp
@@ -0,0 +1,156 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define per(i,n) for(int i=n-1;i>=0;--i)
+#define repn(i,n) for(int i=1;i<=n;++i)
+#define rng(i, a, b) for(int i=a; i<b; i++)
+#define gnr(i,a,b) for(int i=b-1;i>=a;--i)
+#define vb vector<bool>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvb vector<vb>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define si unordered_set<int>
+#define sl unordered_set<ll>
+#define tsi set<int>
+#define tsl set<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define tmii map<int, int>
+#define tmll map<ll, ll>
+#define mii unordered_map<int, int>
+#define mll unordered_map<ll, ll>
+#define pqi priority_queue<int>
+#define pqig priority_queue<int, vi, greater<int>>
+#define pql priority_queue<ll>
+#define pqlg priority_queue<ll, vl, greater<ll>>
+#define pqpii priority_queue<pii>
+#define pqpll priority_queue<pll>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int) (x).size()
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define sp ' '
+#define fi first
+#define se second
+#define pb push_back
+#define mpr make_pair
+ 
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << sp; use_(x); cerr << nl;
+#else
+#define debug(x)
+#endif
+
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx[8]={0,1,0,-1,1,1,-1,-1};
+int dy[8]={1,0,-1,0,1,-1,1,-1};
+
+void yesnoc(bool a) {
+    cout << (a? "YES\n": "NO\n");
+}
+void yesnos(bool a) {
+    cout << (a? "Yes\n": "No\n");
+}
+void use_(ll W) {cerr << W;}
+void use_(ld W) {cerr << W;}
+void use_(char W) {cerr << W;}
+void use_(string W) {cerr << W;}
+void use_(int W) {cerr << W;}
+void use_(double W) {cerr << W;}
+
+template<typename T, typename V> void use_(pair<T, V> p) {
+    cerr << "["; use_(p.fi); cerr << ','; use_(p.se); cerr << ']';
+}
+template<typename T> void use_(vector<T> a) {
+    cerr << '['; for(T i: a) { use_(i); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+template<typename T> void use_(set<T> a) {
+    cerr << '['; for(T i: a) { use_(i); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+template<typename T> void use_(multiset<T> ms) {
+    cerr << '['; for(T i: ms) { use_(i); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+template<typename T, typename V> void use_(map<T, V> mp) {
+    cerr << '[';  for(auto x: mp) { use_(x.fi); cerr << ','; 
+        use_(x.se); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+ 
+int t, n;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // what are all the sums possible using given coins
+    int n; 
+    cin >> n;
+    vi a(n + 1);
+    repn(i, n) cin >> a[i];
+    ll sum = accumulate(all(a), 0ll);
+    debug(sum);
+    vvi possible(sum + 1, vi(n + 1, 0));
+    possible[0][0] = 1;
+    repn(i, n) {
+        rep(j, sum + 1) {
+            if(j - a[i] >= 0) possible[j][i] |= possible[j-a[i]][i-1];
+            possible[j][i] |= possible[j][i-1];
+        }
+    }
+
+    tsi res;
+    rep(i, sum + 1) {
+        repn(j, n) {
+            if(possible[i][j]) res.insert(i);
+        }
+    }
+    debug(res);
+    for(auto x: res) cout << x << sp;
+
+    cout << "Can we make sum using all ?";
+    cout << possible[sum][n] << nl;
+    //using O(n) space
+    vi can(sum + 1, 0);
+    can[0] = 1;
+    repn(i, n) {
+        for(ll j = sum; j >= 0; --j) {
+            if(can[j]) can[j + a[i]] = 1;
+        }
+    }
+    rep(i, sum + 1) {
+        if(can[i]) cout << i << sp;
+    }
+
+
+}
