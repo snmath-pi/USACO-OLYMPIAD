bf1b2a18af8bfd2703d5bbc6c65cd75042ca033d
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-03-17 09:44:32 +0530

    Update Addition.cpp

--- SilverMedal/IntroToBitwiseOperators/Addition.cpp
+++ SilverMedal/IntroToBitwiseOperators/Addition.cpp
@@ -1,61 +1,20 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-void solve() {
-	int n; cin >> n;
-	string s; cin >> s;
-	int m = s.size();
-	if(s[0] == s[m-1]) {
-		int ok = 0;
-		for(int l=1, r = m-2; l < r; l++, r-- ){
-			if(s[l] > s[r]) ok = 1;
-		}
-		if(ok) {
-			if(n % 2) {
-				reverse(begin(s), end(s));
-				cout << s << '\n';
-			} else {
-				string t = s;
-				reverse(begin(t), end(t));
-				cout << t + s << '\n';
-			}
-		} else {
-			if(n % 2) {
-				string t = s;
-				reverse(begin(t), end(t));
-				cout << s + t << '\n';;
-			} else {
-				cout << s << '\n';
-			}
-		}
-		return;
+int add(int x, int y) {
+	while(y) {
+		int carry = x & y;
+		x = x ^ y;
+		y = carry << 1;
 	}
-	if(s[0] < s[m-1]) {
-		if(n % 2 == 0) {
-			cout << s << '\n';
-		} else {
-			string t = s; reverse(begin(t), end(t));
-			cout << s + t << '\n';
-		}
-	} else {
-
-		string t = s; reverse(begin(t), end(t));
-		if(n % 2 == 0) {
-			cout << t + s << '\n';
-		} else {
-			cout << t << '\n';
-		}
-	}
+	return x;
 }
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
-	int T; cin >> T;
-	for(;T--;) {
-		solve();
-	}	
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << add(a, b) << '\n';
 }
